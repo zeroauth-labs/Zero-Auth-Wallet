5e7fd6353ddd20df88468f7695277b0bba136608
Nidhin Xavier <132472527+NDX7@users.noreply.github.com>
2026-02-09 20:34:51 +0530

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (api) {
-    api.cache(false);
-    return {
-        presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }]],
-        plugins: ['react-native-reanimated/plugin'],
-    };
-};
